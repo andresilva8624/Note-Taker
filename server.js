ae99345f699b20cf2730ff7408c5cd715acec114
Andre <andresilva8624@gmail.com>
2022-09-26 09:39:54 -0700

    update

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/api/notes', (req,res) =>{
+    // Refer to folder 22 Solved, route/tip.js //
+})
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
