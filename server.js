549bd18171a0c28e91b654109981b8470ce431be
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-09-30 15:06:34 -0700

    udpated server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 
 const app = express();
 const PORT = 3001;
