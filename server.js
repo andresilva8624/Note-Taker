36bb80c53a583c454bce51df074666eb1e855626
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-10-03 17:48:32 -0700

    updated server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const notes = require('express').Router();
 const path = require('path');
 const { readFromFile, readAndAppend, writeToFile } = require('./helpers/fsUtils.js');
+const uuid = require('./helpers/uuid.js');
 
 
 
@@ -16,43 +18,46 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/api/notes', (req,res) =>{
-  readFromFile('./db/db.json').then((data) =>res.json(JSON.parse(data)))
-    // Refer to folder 22 Solved, route/note.js //
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+  // Refer to folder 22 Solved, route/note.js //
 });
 
-app.post('/api/notes', (req,res) => {
+app.post('/api/notes', (req, res) => {
 
 
   const { title, text } = req.body;
-console.log(req.body)
+  console.log(req.body)
   if (req.body) {
     const newNote = {
       title,
-      text
+      text,
+      note_id: uuid(),
     };
-
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully 🚀`);
   } else {
     res.error('Error in adding note');
-  }
-  app.delete('/:note_id', (req, res) => {
+  };
+
+
+
+  app.delete('api/notes/:note_id', (req, res) => {
     const noteId = req.params.note_id;
     readFromFile('./db/db.json')
       .then((data) => JSON.parse(data))
       .then((json) => {
         // Make a new array of all notes except the one with the ID provided in the URL
         const result = json.filter((note) => note.note_id !== noteId);
-  
+
         // Save that array to the filesystem
         writeToFile('./db/db.json', result);
-  
+
         // Respond to the DELETE request
         res.json(`Item ${noteId} has been deleted 🗑️`);
       });
   });
-  
+
 });
 
 app.get('*', (req, res) =>
@@ -65,4 +70,5 @@ app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
 
+module.exports = notes;
 
