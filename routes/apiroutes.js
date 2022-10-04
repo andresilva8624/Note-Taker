4df6250137bda24dc1127bc69003c303c982c2bd
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-10-03 21:27:14 -0700

    updated routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,45 @@
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils.js')
+const notes = require('express').Router();
+const uuid  = require('../helpers/uuid.js')
+
+
+notes.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    // Refer to folder 22 Solved, route/note.js //
+});
+
+notes.post('/notes', (req, res) => {
+
+
+    const { title, text } = req.body;
+    console.log(req.body)
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding note');
+    };
+});
+
+    notes.delete('/notes/:note_id', (req, res) => {
+        const noteId = req.params.note_id;
+        readFromFile('./db/db.json')
+            .then((data) => JSON.parse(data))
+            .then((json) => {
+                // Make a new array of all notes except the one with the ID provided in the URL
+                const result = json.filter((note) => note.note_id !== noteId);
+
+                // Save that array to the filesystem
+                writeToFile('./db/db.json', result);
+
+                // Respond to the DELETE request
+                res.json(`Item ${noteId} has been deleted 🗑️`);
+            
+            });
+    });
+module.exports = notes;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const notes = require('express').Router();
 const path = require('path');
 const { readFromFile, readAndAppend, writeToFile } = require('./helpers/fsUtils.js');
 const uuid = require('./helpers/uuid.js');
-
+const api = require('./routes/apiroutes');
 
 
 const app = express();
@@ -12,53 +12,13 @@ const PORT = 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api);
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-  // Refer to folder 22 Solved, route/note.js //
-});
 
-app.post('/api/notes', (req, res) => {
-
-
-  const { title, text } = req.body;
-  console.log(req.body)
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-      note_id: uuid(),
-    };
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error('Error in adding note');
-  };
-
-
-
-  app.delete('api/notes/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        // Make a new array of all notes except the one with the ID provided in the URL
-        const result = json.filter((note) => note.note_id !== noteId);
-
-        // Save that array to the filesystem
-        writeToFile('./db/db.json', result);
-
-        // Respond to the DELETE request
-        res.json(`Item ${noteId} has been deleted 🗑️`);
-      });
-  });
-
-});
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
